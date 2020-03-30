7ced3e054ff9d9c97ede0d4585cce717646d3009
jmschwei <jmschwei@users.noreply.github.com>
2020-03-30 15:12:52 -0700

    updating

--- TESTS/test_connect3.py
+++ TESTS/test_connect3.py
@@ -1,23 +1,16 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf610
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+from netmiko import ConnectHandler
+from getpass import getpass
 
-\f0\fs24 \cf0   1 from netmiko import ConnectHandler\
-  2 from getpass import getpass\
-  3\
-  4 device1 = \{\
-  5     "host": 'cisco1.lasthop.io',\
-  6     "username": 'pyclass',\
-  7     "password": getpass(),\
-  8     "device_type": 'cisco_ios',\
-  9     #"global_delay_factor": 2,\
- 10 \}\
- 11\
- 12 net_connect = ConnectHandler(**device1)\
- 13 print(net_connect.find_prompt())\
- 14\
- 15 output = net_connect.send_command("show ip int brief", delay_factor=4, max_loops=1000)\
- 16 print(output)}
+	device1 = {
+		"host": 'cisco1.lasthop.io',
+		"username": 'pyclass',
+		"password": getpass(),
+		"device_type": 'cisco_ios',
+		#"global_delay_factor": 2,
+		}
+
+	net_connect = ConnectHandler(**device1)
+	print(net_connect.find_prompt())
+
+	output = net_connect.send_command("show ip int brief", delay_factor=4, max_loops=1000)
+	print(output)}
